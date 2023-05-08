e5cfba820e6791cc18ae4a901edbe85a7489bbfe
Adriano Anschau <adrianoanschau@gmail.com>
2023-05-08 17:29:48 -0300

    setup express

--- amazonia-api/src/index.ts
+++ amazonia-api/src/index.ts
@@ -1,1 +1,13 @@
-// Amazonia-API Project Starts
+import express from 'express';
+
+const HOST = process.env.HOST || '0.0.0.0';
+const PORT = parseInt(process.env.PORT || '3000', 10);
+
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Amazonia API :: v0.1.0');
+});
+
+app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
