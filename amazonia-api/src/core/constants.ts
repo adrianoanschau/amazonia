f00c9f68f1240a572c5addd2922c1c52eddd6cee
Adriano Anschau <adrianoanschau@gmail.com>
2023-05-08 21:33:44 -0300

    refact: create horizontal and vertical locations as a core constant

---
+++ amazonia-api/src/core/constants.ts
@@ -0,0 +1,2 @@
+export const horizontalLocations = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+export const verticalLocations = ['1', '2', '3', '4', '5', '6', '7', '8'];
--- amazonia-api/src/validation/delivery-time.schema.ts
+++ amazonia-api/src/validation/delivery-time.schema.ts
@@ -1,77 +1,15 @@
 import Joi from 'joi';
-
-const locationValues = [
-  'A1',
-  'A2',
-  'A3',
-  'A4',
-  'A5',
-  'A6',
-  'A7',
-  'A8',
-  'B1',
-  'B2',
-  'B3',
-  'B4',
-  'B5',
-  'B6',
-  'B7',
-  'B8',
-  'C1',
-  'C2',
-  'C3',
-  'C4',
-  'C5',
-  'C6',
-  'C7',
-  'C8',
-  'D1',
-  'D2',
-  'D3',
-  'D4',
-  'D5',
-  'D6',
-  'D7',
-  'D8',
-  'E1',
-  'E2',
-  'E3',
-  'E4',
-  'E5',
-  'E6',
-  'E7',
-  'E8',
-  'F1',
-  'F2',
-  'F3',
-  'F4',
-  'F5',
-  'F6',
-  'F7',
-  'F8',
-  'G1',
-  'G2',
-  'G3',
-  'G4',
-  'G5',
-  'G6',
-  'G7',
-  'G8',
-  'H1',
-  'H2',
-  'H3',
-  'H4',
-  'H5',
-  'H6',
-  'H7',
-  'H8',
-];
+import { horizontalLocations, verticalLocations } from '../core/constants';
 
 export type DeliveryTimeRequest = {
   start_on: string;
   object_location: string;
   delivery_on: string;
 };
+
+const locationValues = horizontalLocations
+  .map((h) => verticalLocations.map((v) => `${h}${v}`))
+  .flat();
 
 export const deliveryTimeSchema = Joi.object<DeliveryTimeRequest>().keys({
   start_on: Joi.string()
