4bbb5372d2b5f6767d33913a691eeb6b65b307f8
Adriano Anschau <adrianoanschau@gmail.com>
2023-05-09 21:25:10 -0300

    feat: create health check test integration

--- amazonia-api/tests/health-check.spec.ts
+++ amazonia-api/tests/health-check.spec.ts
@@ -1,18 +1,25 @@
 import request from 'supertest';
-import main from '../src/main';
 import packageJson from '../package.json';
+import { App } from '../src/app';
+import appMock from './mocks/app.mock';
 
-describe('Health Check', () => {
+describe('Health Check (Root) endpoint', () => {
+  let app: App;
+
+  beforeAll(async () => {
+    app = await appMock();
+  });
+
   it('GET / => app name and version', async () => {
     const appName = process.env.APP_NAME || 'App';
     const appVersion = packageJson.version;
 
-    return request(main.application)
+    return request(app.application)
       .get('/')
-      .expect('Content-Type', /json/)
       .expect(200)
+      .expect('Content-Type', /json/)
       .then((response) => {
-        expect(response.body).toEqual({
+        expect(response.body).toStrictEqual({
           name: appName,
           version: `v${appVersion}`,
         });
